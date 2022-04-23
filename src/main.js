fed64a9827f07f3ccb635512212dcbdb0a8e9f4c
stela.josan <stela.josan@stud.acs.upb.ro>
2022-04-23 22:17:06 +0300

    router added

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import JsonExcel from "../node_modules/vue-json-excel";
 import VueJsonToCsv from '../node_modules/vue-json-to-csv'
+import router from './router'
 Vue.component("downloadExcel", JsonExcel);
 Vue.component("vue-json-to-csv", VueJsonToCsv);
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
