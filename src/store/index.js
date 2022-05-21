15fdced496afb9e30d5e8381489560af6fcc5f4f
Andreea Paiu <paiuandreea18@yahoo.com>
2022-05-21 09:38:31 +0300

    c

--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    city: null
   },
   getters: {
   },
   mutations: {
-    update_city(state, payload){
-      localStorage.setItem("city", payload);
-      state.city = payload;
-    }
   },
   actions: {
-    add_city({commit}, city){
-      this.state.city = city;
-      commit("update_city", city);
-    }
   },
   modules: {
   }
