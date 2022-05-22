b97d8a9b6471bad6c8d86c85f1b5ae7c74db2ae0
stela.josan <stela.josan@stud.acs.upb.ro>
2022-05-22 21:56:00 +0300

    delete proxy

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
-    proxy: 'http://localhost:8098',
-    },
   transpileDependencies: [
     'vuetify'
   ]
